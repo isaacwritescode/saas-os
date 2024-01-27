1657dc617e2bc7c285ee057a3cacd94ecebbb762
Isaac Fernandes <isaacwritescode@protonmail.com>
2024-01-27 11:18:15 +0530

    make sponsor section responsive

--- src/pages/Sponsors/index.js
+++ src/pages/Sponsors/index.js
@@ -3,8 +3,14 @@ import SPONSORS from "./constants";
 
 const Sponsors = () => {
   return (
-    <Box maxWidth={1000} width="100%" m="auto">
-      <Stack py={6} direction="row" justifyContent="space-between">
+    <Box maxWidth={1000} width={{ xs: "90%", md: "100%" }} m="auto">
+      <Stack
+        py={6}
+        gap={4}
+        direction="row"
+        justifyContent={{ md: "space-between" }}
+        flexWrap="wrap"
+      >
         {SPONSORS.map(({ src }, idx) => (
           <img src={src} idx={idx} />
         ))}

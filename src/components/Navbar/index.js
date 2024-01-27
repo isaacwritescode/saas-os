4a0283c2803fb3e072340b2cb96c0d877e59aa35
Isaac Fernandes <isaacwritescode@protonmail.com>
2024-01-27 11:14:20 +0530

    make navbar responsive

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,11 +2,19 @@ import { Box, Button, Stack } from "@mui/material";
 
 export default function Navbar() {
   return (
-    <Box position="sticky" top={0} bgcolor="white" borderBottom={1} borderColor="grey.300">
-      <Box width="70%" m="auto" py={1}>
+    <Box
+      position="sticky"
+      top={0}
+      bgcolor="white"
+      borderBottom={1}
+      borderColor="grey.300"
+    >
+      <Box width={{ xs: "90%", md: "70%" }} m="auto" py={1}>
         <Stack direction="row" justifyContent="space-between">
           <img src="logo.svg" height={36} alt="logo" />
-          <Button size="small" variant="contained">Join waitlist</Button>
+          <Button size="small" variant="contained">
+            Join waitlist
+          </Button>
         </Stack>
       </Box>
     </Box>

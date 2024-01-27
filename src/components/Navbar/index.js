4f158810882d105fb714b5e209e8e269babcb9a2
Isaac Fernandes <isaacwritescode@protonmail.com>
2024-01-27 11:49:02 +0530

    make hero section responsive

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -8,6 +8,7 @@ export default function Navbar() {
       bgcolor="white"
       borderBottom={1}
       borderColor="grey.300"
+      zIndex={999}
     >
       <Box width={{ xs: "90%", md: "70%" }} m="auto" py={1}>
         <Stack direction="row" justifyContent="space-between">
--- src/pages/Hero/index.js
+++ src/pages/Hero/index.js
@@ -1,12 +1,11 @@
-import { Masonry } from "@mui/lab";
 import { Box, Button, Grid, Stack, Typography } from "@mui/material";
 
 const Hero = () => {
   return (
-    <Box bgcolor="primary.light">
+    <Box bgcolor="primary.light" sx={{backgroundImage: "url(/hero/illus/boxes.svg)"}}>
       <Box width={{ xs: "90%", lg: "80%" }} m="auto" maxWidth={1000}>
-        <Stack spacing={4} alignItems="center" py={18}>
-          <Stack spacing={2} textAlign="center">
+        <Stack spacing={4} alignItems={{ xs: "start", md: "center" }} py={18}>
+          <Stack spacing={2} textAlign={{ xs: "left", md: "center" }}>
             <Typography variant="h1">
               Design SaaS products in minutes
             </Typography>
@@ -22,16 +21,37 @@ const Hero = () => {
             Join waitlist
           </Button>
         </Stack>
-        <Box >
-          <Grid container spacing={3} sx={{aspectRatio: "4/1.3", overflow: "hidden", width: "100%"}}>
+        <Box>
+          <Grid
+            container
+            display={{ xs: "flex", md: "none" }}
+            spacing={{ xs: 1.5, md: 3 }}
+            sx={{ aspectRatio: "4/1.8", overflow: "hidden", width: "100%" }}
+          >
+            <Grid item xs={4}>
+              <Stack spacing={{ xs: 1.5, md: 3 }}>
+                <img src="/hero/illus/05.svg" width="100%" />
+                <img src="/hero/illus/04.svg" width="100%" />
+              </Stack>
+            </Grid>
+            <Grid item xs={8}>
+              <img src="/hero/illus/02.svg" width="100%" />
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            display={{ xs: "none", md: "flex" }}
+            spacing={{ xs: 1.5, md: 3 }}
+            sx={{ aspectRatio: "4/1.3", overflow: "hidden", width: "100%" }}
+          >
             <Grid item xs={3}>
-              <Stack spacing={3}>
+              <Stack spacing={{ xs: 1.5, md: 3 }}>
                 <img src="/hero/illus/05.svg" width="100%" />
                 <img src="/hero/illus/04.svg" width="100%" />
               </Stack>
             </Grid>
             <Grid item xs={3}>
-              <Stack spacing={3}>
+              <Stack spacing={{ xs: 1.5, md: 3 }}>
                 <img src="/hero/illus/03.svg" width="100%" />
                 <img src="/hero/illus/06.svg" width="100%" />
               </Stack>

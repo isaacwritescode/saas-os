c3d9b91e415ea3d1f102f062873f06bf38c5fe38
Isaac Fernandes <isaacwritescode@protonmail.com>
2024-01-27 11:13:36 +0530

    make projects section responsive

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -3,21 +3,21 @@ import PROJECTS from "./constants";
 
 const Projects = () => {
   return (
-    <Box maxWidth={1000} width="100%" mx="auto" mb={16}>
+    <Box maxWidth={1000} width={{ xs: "90%", md: "100%" }} mx="auto" mb={16}>
       <Stack spacing={1} mb={8} textAlign="center">
         <Typography variant="h2">What would you like to build?</Typography>
         <Typography variant="body1">
           See what you can build with SaaS OS
         </Typography>
       </Stack>
-      <Grid container spacing={3}>
+      <Grid container spacing={3} columns={{ xs: 4, md: 8 }}>
         {PROJECTS.map(({ title, desc, src }, idx) => {
           return (
             <Grid item xs={4}>
               <Stack spacing={1} key={idx}>
-                  <img src={src} />
+                <img src={src} />
                 <Typography variant="h6">{title}</Typography>
-                <Typography variant="body1">{desc}</Typography>
+                <Typography variant="body1" color="text.secondary">{desc}</Typography>
               </Stack>
             </Grid>
           );

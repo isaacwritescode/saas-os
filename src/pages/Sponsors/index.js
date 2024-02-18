e1c1f6acec03282d80a110041a9d8eb1bd1779d3
Isaac Fernandes <isaacwritescode@protonmail.com>
2024-02-18 20:03:16 +0530

    sponsor horizontal scrolling

--- src/pages/Sponsors/index.js
+++ src/pages/Sponsors/index.js
@@ -1,20 +1,43 @@
 import { Box, Stack } from "@mui/material";
 import SPONSORS from "./constants";
+import Marquee from "react-fast-marquee";
 
 const Sponsors = () => {
   return (
-    <Box maxWidth={1000} width={{ xs: "90%", md: "100%" }} m="auto">
-      <Stack
-        py={6}
-        gap={4}
-        direction="row"
-        justifyContent={{ md: "space-between" }}
-        flexWrap="wrap"
-      >
-        {SPONSORS.map(({ src }, idx) => (
-          <img src={src} idx={idx} />
-        ))}
-      </Stack>
+    <Box bgcolor="white">
+      <Box maxWidth={1000} width="100%" m="auto">
+        <Stack
+          py={8}
+          gap={4}
+          direction="row"
+          flexWrap="wrap"
+          alignItems="center"
+          justifyContent={{ md: "space-between" }}
+        >
+          <Marquee>
+            {SPONSORS.map(({ src }, idx) => (
+              <Box px={4}>
+                <img
+                  src={src}
+                  idx={idx}
+                  alt={"sponsor"}
+                  style={{ filter: "grayscale(1)" }}
+                />
+              </Box>
+            ))}
+            {SPONSORS.map(({ src }, idx) => (
+              <Box px={4}>
+                <img
+                  src={src}
+                  idx={idx}
+                  alt={"sponsor"}
+                  style={{ filter: "grayscale(1)" }}
+                />
+              </Box>
+            ))}
+          </Marquee>
+        </Stack>
+      </Box>
     </Box>
   );
 };

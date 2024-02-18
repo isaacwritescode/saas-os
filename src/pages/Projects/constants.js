2a14a57ecd823e213052da56d1ad39a9e3799f13
Isaac Fernandes <isaacwritescode@protonmail.com>
2024-02-18 19:50:16 +0530

    projects section hover effects

--- src/pages/Projects/constants.js
+++ src/pages/Projects/constants.js
@@ -2,17 +2,17 @@ const PROJECTS = [
   {
     title: "Scheduling Platform",
     desc: "This is a sample short description",
-    src: "/projects/Scheduling Platform.png",
+    src: "/projects/Scheduling Platform.jpg",
   },
   {
     title: "Headless Commerce",
     desc: "This is a sample short description",
-    src: "/projects/Headless Commerce.png",
+    src: "/projects/Headless Commerce.jpg",
   },
   {
     title: "CRM",
     desc: "This is a sample short description",
-    src: "/projects/CRM.png",
+    src: "/projects/CRM.jpg",
   },
 ];
 
--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -13,16 +13,36 @@ const Projects = () => {
     >
       <Stack spacing={1} mb={8} textAlign="center">
         <Typography variant="h2">What would you like to build?</Typography>
-        <Typography variant="body1">
+        <Typography variant="body1" color="text.secondary">
           See what you can build with SaaS OS
         </Typography>
       </Stack>
       <Grid container spacing={3} columns={{ xs: 4, sm: 8, md: 12 }}>
         {PROJECTS.map(({ title, desc, src }, idx) => {
           return (
-            <Grid item xs={4}>
-              <Stack spacing={2} key={idx}>
-                <img src={src} />
+            <Grid key={idx} item xs={4}>
+              <Stack
+                spacing={2}
+                sx={{
+                  "&:hover img": {
+                    transform: "scale(0.8) !important",
+                  },
+                  cursor: "pointer",
+                }}
+              >
+                <Box bgcolor="#f2f3f5" flex={1} borderRadius={2}>
+                  <img
+                    src={src}
+                    alt={title}
+                    width="100%"
+                    style={{
+                      transform: "scale(0.7)",
+                      transition: "all ease 0.3s",
+                      boxShadow: "8px 8px 24px 8px #00000020",
+                      borderRadius: 8,
+                    }}
+                  />
+                </Box>
                 <Typography textAlign="center" variant="h6">
                   {title}
                 </Typography>
